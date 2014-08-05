5a9376cb356730b605dba06235feb3454098db51
hirata.motoi <hirata.motoi@gmail.com>
2014-08-05 17:35:55 +0900

    add PageViewController

---
+++ babyry/PageViewController.h
@@ -0,0 +1,13 @@
+//
+//  PageViewController.h
+//  babyry
+//
+//  Created by hirata.motoi on 2014/08/05.
+//  Copyright (c) 2014年 jp.co.meaning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageViewController : UIPageViewController
+
+@end
