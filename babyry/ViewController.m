430fef31661686899b8e5445e60f94d16ac85541
kenji.suzuki <kenji.suzuki.1984@gmail.com>
2014-08-07 16:45:54 +0900

    change thumbnail size

--- babyry/ViewController.m
+++ babyry/ViewController.m
@@ -37,7 +37,7 @@
     //NSLog(@"viewDidLoad");
     
     // よく使うからここに書いておく
-    [PFUser logOut];
+    //[PFUser logOut];
     
     _hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
     _hud.labelText = @"データ準備中";
