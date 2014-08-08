f2d2977833db6e6dab2f4125e46c949e11aab5d5
hirata.motoi <hirata.motoi@gmail.com>
2014-08-08 19:37:08 +0900

    Added a module to handle partner user object

---
+++ babyry/Partner.h
@@ -0,0 +1,17 @@
+//
+//  Partner.h
+//  babyry
+//
+//  Created by hirata.motoi on 2014/08/08.
+//  Copyright (c) 2014年 jp.co.meaning. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Parse/Parse.h>
+
+@interface Partner : NSObject
+
++ (Partner *)partnerUser;
++ (void)initialize;
+    
+@end
