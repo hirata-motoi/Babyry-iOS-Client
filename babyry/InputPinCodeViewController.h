ef47879fba8e5aaed32aaab7b211378cc4242891
kenji.suzuki <kenji.suzuki.1984@gmail.com>
2014-09-21 13:30:39 +0900

    add pincode view

---
+++ babyry/InputPinCodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  InputPinCodeViewController.h
+//  babyry
+//
+//  Created by Kenji Suzuki on 2014/09/19.
+//  Copyright (c) 2014年 jp.co.meaning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InputPinCodeViewController : UIViewController
+
+@end
