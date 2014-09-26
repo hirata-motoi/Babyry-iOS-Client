19acda2888f19a2021e3e4464d5ed62f1dd271b0
hirata.motoi <hirata.motoi@gmail.com>
2014-09-26 16:09:45 +0900

    Added header view

---
+++ babyry/TutorialSentApplyView.h
@@ -0,0 +1,16 @@
+//
+//  TutorialSentApplyView.h
+//  babyry
+//
+//  Created by hirata.motoi on 2014/09/26.
+//  Copyright (c) 2014年 jp.co.meaning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TutorialSentApplyView : UIView
+
+@property (weak, nonatomic) IBOutlet UIButton *openPartnerApplyListButton;
++ (instancetype)view;
+
+@end
