7c7f87f400885156fbe62668d7cfff0bc44395fe
kenji.suzuki <kenji.suzuki.1984@gmail.com>
2014-10-09 16:06:42 +0900

    remove familyApply in normal tutorial step

--- babyry/PageContentViewController.m
+++ babyry/PageContentViewController.m
@@ -156,8 +156,9 @@
  
     // ベストショット選択を促すとき(chooseByUser)と写真のアップロードを促す時(uploadByUser)は
     // cellにholeをあてるためcell表示後にoverlayを出す必要がある
+    // familyApplyは非同期でheader viewを出した後にチュートリアルを表示しているので、ここでも表示するとちかちかする
     TutorialStage *currentStage = [Tutorial currentStage];
-    if ( !([currentStage.currentStage isEqualToString:@"chooseByUser"] || [currentStage.currentStage isEqualToString:@"uploadByUser"]) ) {
+    if ( !([currentStage.currentStage isEqualToString:@"chooseByUser"] || [currentStage.currentStage isEqualToString:@"uploadByUser"] || [currentStage.currentStage isEqualToString:@"familyApply"]) ) {
         [self showTutorialNavigator];
     }
 }
